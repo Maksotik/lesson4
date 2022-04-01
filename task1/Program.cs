5fb10724d8be18b95ff81b1f52827e4519e36a3e
Maksotik <Maksotik@rambler.ru>
2022-04-01 13:32:08 +0300

    Задача1 возведение числа в степень

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+void num_pow(int num, int pow){
+int deg = 1;    
+    if (num == 0){
+        Console.WriteLine("Результат возведения в степень: ");
+        Console.WriteLine("{0}", deg*0);
+        return;
+    }
+    for( int i = 0; i < pow; i++ ){
+            deg *= num;
+    }
+    Console.WriteLine("Результат возведения в степень: ");
+    Console.WriteLine(deg);
+}
+
+
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральную степень:  ");
+int pow = int.Parse(Console.ReadLine());
+num_pow(number, pow);
+
