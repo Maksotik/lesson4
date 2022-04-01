13a0a8d77d66e7dbed6cf469d48bca2517f864cd
Maksotik <Maksotik@rambler.ru>
2022-04-01 21:46:00 +0300

    Задача3 заполнение массива случайными числами

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов 
+// случайными числами и выводит их на экран. 
+// Оформите заполнение массива и вывод в виде функции
+
+int size = 8;
+int [] arr = new int [size];
+Random rand = new Random();
+
+for( int i = 0; i < size; i++ ){
+    arr[i] = rand.Next(0, 9);
+    Console.Write("{0} ", arr[i]); 
+}
+Console.WriteLine();
+
+
+
