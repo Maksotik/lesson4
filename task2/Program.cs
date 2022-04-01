77d281055b9b69d82f4d5a371f059443220a5156
Maksotik <Maksotik@rambler.ru>
2022-04-01 14:09:01 +0300

    Задача2 сумма цифр в числе

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void sum (int num){
+int sum = 0;    
+    while( num != 0 ){
+        sum += (num % 10);
+        num /= 10;
+    }
+    Console.WriteLine("Сумма цифр в числе - {0}", sum);
+}
+
+
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+sum(number);
